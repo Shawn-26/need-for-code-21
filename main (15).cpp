190b5ef1357c0ad8804ecb5646e0760ae5c77e0f
Mayank Kalbande <83835663+mdkalbande@users.noreply.github.com>
2022-07-26 11:36:35 +0530

    Update main (15).cpp

--- main (15).cpp
+++ main (15).cpp
@@ -1,16 +1,2 @@
 #include<iostream>
 using namespace std;
-int main(){
-	long long int t,n;
-	cin>>t;
-	while(t--){
-		cin>>n;
-		cout<<2<<endl;
-		cout<<n<<" "<<n-1<<endl;
-		while(n-2>0){
-			cout<<n<<" "<<n-2<<endl;
-			n=n-1;
-		}
-	}
-	return 0;
-}
